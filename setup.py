3d932f21e345021e3b54746e9da6ba1190420736
namannaUVA <107503129+namannaUVA@users.noreply.github.com>
2022-07-11 13:37:50 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bookloverPackage',
+      version='0.1',
+      description='HW 14 package',
+      url='',
+      author='Nikita Amanna',
+      author_email='vwr7bc@virginia.edu',
+      license='MIT',
+      packages=['HW14'])
